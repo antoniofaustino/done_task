37fda5a79b2bc87be49b2e887aec66f7712a9674
Antonio Faustino <antoniofaustino12369@gmail.com>
2020-01-23 17:38:49 +0000

    Updated Manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,7 @@
     'name': "done_task",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module to update stage_id of child tasks""",
 
     'description': """
         Long description of module's purpose
